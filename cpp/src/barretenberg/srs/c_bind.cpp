03060b05227fb8ea386be649611b40b9fc9d4180
Bartosz Nowak <bartekn337@gmail.com>
2023-10-09 15:53:41 +0200

    srs_init export

--- cpp/src/barretenberg/srs/c_bind.cpp
+++ cpp/src/barretenberg/srs/c_bind.cpp
@@ -12,13 +12,25 @@ using namespace barretenberg;
  * WARNING: The SRS is not encoded the same way as all the read/write methods encode.
  * Have to use the old school io functions to parse the buffers.
  */
-WASM_EXPORT void srs_init_srs(uint8_t const* points_buf, uint32_t const* num_points, uint8_t const* g2_point_buf)
+
+extern "C"
 {
-    auto points = std::vector<g1::affine_element>(ntohl(*num_points));
-    srs::IO<curve::BN254>::read_affine_elements_from_buffer(points.data(), (char*)points_buf, points.size() * 64);
+const char * srs_init_srs(uint8_t const* points_buf, uint32_t const* num_points, uint8_t const* g2_point_buf)
+{
+    try
+    {
+        auto points = std::vector<g1::affine_element>(*num_points);
+        srs::IO<curve::BN254>::read_affine_elements_from_buffer(points.data(), (char*)points_buf, points.size() * 64);
 
-    g2::affine_element g2_point;
-    srs::IO<curve::BN254>::read_affine_elements_from_buffer(&g2_point, (char*)g2_point_buf, 128);
+        g2::affine_element g2_point;
+        srs::IO<curve::BN254>::read_affine_elements_from_buffer(&g2_point, (char*)g2_point_buf, 128);
 
-    barretenberg::srs::init_crs_factory(points, g2_point);
+        barretenberg::srs::init_crs_factory(points, g2_point);
+        return nullptr;
+    }
+    catch (const std::exception &e)
+    {
+        return e.what(); // return the exception message
+    }
+}
 }
